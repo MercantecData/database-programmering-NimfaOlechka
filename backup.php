cb0c468a26c9a9ed8df634d1ae9fb693d3265f61
Olga K <nimfaolya@mail.ru>
2017-12-07 12:06:48 +0100

    backup funktion

---
+++ backup.php
@@ -0,0 +1,24 @@
+
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Back Up</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+	</head>
+	<body>
+		
+		<div class="header">
+			<h2> Register </h2>
+		</div>
+		<form class="input_form" method="post" action="backup.php">
+
+			<?php include('errors.php'); ?>
+			<div class="input">
+				<input type="Submit" name="backup" class='btn' value="Save it!">
+			</div>
+		</form>
+
+	</body>
+</html>
