90a311b35f3bb390f19ed1f649ee528ce4cc9e19
Olga K <nimfaolya@mail.ru>
2017-12-07 10:29:38 +0100

    new version of registration

--- register.php
+++ register.php
@@ -1,76 +1,5 @@
-<?php
 
-
-//check of button was activated
-if (isset($_POST['register']))
-{
-		$uname = $_POST['uname'];
-		$pass1 = $_POST["psw"];
-		
-
-	$conn = mysqli_connect('localhost', 'root', '', 'mydb');
-
-	if($conn === false){
-		die ("No connection").mysqli_connect_error();
-	}
-
-	$query = "INSERT INTO `users`(`name`, `password`) VALUES ('$uname','$pass1')";
-
-	$success= mysqli_query($conn,$query);
-
-	if ($success)
-		{
-			echo "Great work!";
-		}else
-		{
-			echo"Pain on you!"; 
-		}
-
-		mysqli_close($conn);
-}
-
-if (isset($_POST['backup']))
-{
-		
-	$connect = mysqli_connect('localhost', 'root', '', 'mydb');
-
-	if($connect === false){
-		die ("No connection").mysqli_connect_error();
-	}
-
-	$query1 = "SELECT * FROM `users`";
-	$result = mysqli_query($connect,$query1);
-
-
-	$json_array= array();
-	while($row = mysqli_fetch_assoc($result))
-	{
-		$json_array[] = $row;
-	}
-
-	$json = json_encode($json_array);
-
-echo $json;
-
-	file_put_contents("myDBBackUp.txt", $json);
-}
-
-if (isset($_POST['login'])){
-	
-$connect2 = mysqli_connect('localhost', 'root', '', 'mydb');
-
-if($connect2 === false){
-		die ("No connection").mysqli_connect_error();
-	}
-
-	$query2 = "SELECT * FROM `users` LEFT JOIN 'test' ON 'users.id'='test.nameID'";
-	$result2 = mysqli_query($connect2,$query2);
-
-
-}
-
-?>
-
+<?php include('server.php') ?>
 
 <!DOCTYPE html>
 <html>
@@ -79,20 +8,29 @@ if($connect2 === false){
 		<link rel="stylesheet" href="style.css" type="text/css">
 	</head>
 	<body>
-		<!--img src="wallpaper.jpg"-->
+		
 		<div class="header">
-			<h2>Register</h2>
+			<h2> Register </h2>
 		</div>
-		<form class="input_form" method="post" action="">
+		<form class="input_form" method="post" action="register.php">
+
+			<?php include('errors.php'); ?>
+
 			<div class="input">
 				<label><b>Name:</b></label>
 					<input type="text" name="uname">
 			</div>
+
 			<div class="input">
 				<label><b>Password:</b></label>
 				<input type="text" name="psw">
 			</div>
 			
+			<div class="input">
+				<label><b> Confirm Password:</b></label>
+				<input type="text" name="cpsw">
+			</div>
+
 			<div class="input">
 				<input type="Submit" name="register" class='btn' value="Add it!">
 			</div>
@@ -100,8 +38,9 @@ if($connect2 === false){
 			<div class="input">
 				<input type="Submit" name="backup" class='btn' value="Save it!">
 			</div>
-			<div class="input">
-				<input type="Submit" name="login" class='btn' value="Log Me In!">
+
+			<div class="input"> I am already registered!
+				<input type="Submit" name="login" class='btn' value="Log Me In!"> <a href="login.php">
 			</div>
 		</form>
 
